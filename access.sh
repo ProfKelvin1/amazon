6c7a3e7cabd7383802c7865d2371aa99d63e06e0
Lara <Lara@yahoo.com>
2023-10-02 12:03:07 -0500

    added an overview of access and script file

---
+++ access.sh
@@ -0,0 +1,4 @@
+this file contains accesses for new hires
+Will will always refer to this file when new employees are hired
+the next set of employees to be hired will resume on the 10th of cotober
+
---
+++ scripts.sh
@@ -0,0 +1,1 @@
+this file will contain standard scripts to be used on calls by every employee on a daily basis 
